10849e825c4b07941bdccec8df6254a95ec8b1aa
Ilya-afk <ivodolazky@mail.ru>
2024-05-30 13:27:50 +0300

    commit

--- mail.py
+++ mail.py
@@ -3,7 +3,7 @@ import re
 
 while True:
     s = input()
-    result = re.fullmatch(f"^['a-zA-Z0-9._-]+@\\S*\\.\\S*$", s)
+    result = re.fullmatch("^['a-zA-Z0-9._-]+@\\S*\\.\\S*$", s)
     if result:
         print('match')
     else:
